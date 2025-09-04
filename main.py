867941f874257d5a840f08c085dda6689dc2993b
Ryan-Utz <rmu5060@psu.edu>
2025-09-04 13:47:59 -0400

    Starting roster input

--- main.py
+++ main.py
@@ -8,7 +8,16 @@ def main():
     label = tk.Label(root, text="Welcome to Fantasy AI", font=("Arial", 24))
     label.pack(pady=20)
 
+    start_button = tk.Button(root, text="Create New Roster", font=("Arial", 18),command=lambda:start_new_roster(root,start_button))
+    start_button.pack(pady=10)
     root.mainloop()
+
+def start_new_roster(Tk_window,button):
+    button.pack_forget()
+    QB_Label = tk.Label(Tk_window,text="Enter number of QBs:",font=("Arial",18))
+    QB_Label.pack(pady=5)
+    QB_Entry = tk.Entry(Tk_window,font=("Arial",18),width=10)
+    QB_Entry.pack(pady=5)
 
 if __name__ == "__main__":
     main()
